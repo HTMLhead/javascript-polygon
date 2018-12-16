f433b250c145d151efc4e5b11cd6ca719d66b784
Head <mrdoo309@gmail.com>
2018-12-16 23:04:43 +0900

    1차 완성
    계산값을 출력해주는 함수들 작성 앞으로 해야할일 => 애니메이션, 백그라운드 넣기

--- index.js
+++ index.js
@@ -19,3 +19,31 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
 }
+
+function calcSquare() {
+  let height = document.getElementById('values-square-height').value
+  let width = document.getElementById('values-square-width').value
+  let result = height * width
+  document.getElementById('square-result').innerHTML = '결과<br>' + result
+}
+
+function calcCircle() {
+  let radius = document.getElementById('values-circle-radius').value
+  let result = (radius * radius * Math.PI).toFixed(2)
+  document.getElementById('circle-result').innerHTML = '결과<br>' + result
+}
+
+function calcTriangle() {
+  let width = document.getElementById('values-triangle-width').value
+  let height = document.getElementById('values-triangle-height').value
+  let result = (width * height) / 2
+  document.getElementById('triangle-result').innerHTML = '결과<br>' + result
+}
+
+function calcTrapezoid() {
+  let height = document.getElementById('values-trapezoid-height').value
+  let topline = Number(document.getElementById('values-trapezoid-topline').value)
+  let bottomline = Number(document.getElementById('values-trapezoid-bottomline').value)
+  let result = ((topline + bottomline) * height) / 2
+  document.getElementById('trapezoid-result').innerHTML = '결과<br>' + result
+}
